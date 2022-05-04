53a7567637384ce8424b810fe3572ccb1a271fb5
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-05-04 10:11:02 +0530

    rename reflow to redditflow

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r") as fh:
 
 install_requires = ["sentence_transformers", "praw"]
 setuptools.setup(
-    name="reflow",
-    version="0.0.2",
+    name="redditflow",
+    version="0.0.1",
     author="Abhijith Neil Abraham",
     author_email="abhijithneilabrahampk@gmail.com",
     description="Data Curation over Time",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='MIT License',
-    url="https://github.com/nfflow/reflow",
+    url="https://github.com/nfflow/redditflow",
     install_requires=install_requires,
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
