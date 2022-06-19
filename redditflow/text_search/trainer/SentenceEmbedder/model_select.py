f0a50f0b2563eeb33814c4ea11066e98840fb8c1
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-06-19 12:30:01 +0530

    Revert "#11 Add more unsupervised models"

--- redditflow/text_search/trainer/SentenceEmbedder/model_select.py
+++ redditflow/text_search/trainer/SentenceEmbedder/model_select.py
@@ -7,23 +7,13 @@ class ModelSelect:
         self.model_output_path = model_output_path
 
     def return_trainer(self):
-        model_architecture = self.model_architecture.lower()
+        model_architecture = self.model_architecture
         model_name = self.model_name
         model_output_path = self.model_output_path
-        if model_architecture == 'ct':
-            from .ct import ContrastiveTensionTrainer
+        if model_architecture == 'CT':
+            from .CT import ContrastiveTensionTrainer
             trainer = ContrastiveTensionTrainer(model_name,
                                                 model_output_path)
-            return trainer
-        elif model_architecture == 'simcse':
-            from .simcse import SimCSETrainer
-            trainer = SimCSETrainer(model_name,
-                                    model_output_path)
-            return trainer
-        elif model_architecture == 'tsdae':
-            from .tsdae import TsdaeTrainer
-            trainer = TsdaeTrainer(model_name,
-                                   model_output_path)
             return trainer
         else:
             print('model architecture not found in available architectures')
--- redditflow/text_search/trainer/__init__.py
+++ redditflow/text_search/trainer/__init__.py
@@ -1,4 +1,2 @@
-from .SentenceEmbedder.ct.contrastive_tension import ContrastiveTensionTrainer
-from .SentenceEmbedder.simcse.simcse import SimCSETrainer
-from .SentenceEmbedder.tsdae.tsdae import TsdaeTrainer
+from .SentenceEmbedder.CT.contrastive_tension import ContrastiveTensionTrainer
 from .SentenceEmbedder.model_select import ModelSelect
