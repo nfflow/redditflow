8fad6a30e02b2a1231202f9a668a7b9c9580d7ff
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-06-26 22:26:18 +0530

    add resume_task_timestamp

--- redditflow/apis/text/TextApi.py
+++ redditflow/apis/text/TextApi.py
@@ -45,6 +45,8 @@ class TextApi:
             else:
                 trainer = ModelSelect(model_name,
                                       model_output_path).return_trainer()
+            if 'resume_task_timestamp' in config.keys():
+                save_timestamp = config['resume_task_timestamp']
 
             trainer.train(data_path=os.path.join(
                 save_timestamp, 'scraped_classified.json'))
